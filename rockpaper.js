1f65bfc7d73859e00bb54da9c19c991d12f548ad
iknowkeith <keith@iknowkeith.com>
2014-02-07 16:04:09 -0500

    Update rockpaper.js

--- rockpaper.js
+++ rockpaper.js
@@ -1,6 +1,5 @@
 var userChoice = prompt("Do you choose rock, paper or scissors?");
 var computerChoice = Math.random();
-console.log(computerChoice);
 if (computerChoice < 0.34) {
 	computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
@@ -8,6 +7,7 @@ if (computerChoice < 0.34) {
 } else {
 	computerChoice = "scissors";
 }
+
 var compare = function(choice1, choice2){
     if (choice1 === choice2){
         return "The result is a tie!";
@@ -22,10 +22,20 @@ var compare = function(choice1, choice2){
     }
     if (choice1==="paper") {
         if (choice2==="rock") {
-            return "Paper Wins";
+            return "Paper Wins!";
         } 
         else{
-            return "Scissors Wins";
+            return "Scissors Wins!";
+        }
+    }
+    else if(choice1==="scissors"){
+        if(choice2==="rock"){
+            return "Rock wins!";
+        }
+        else if(choice2 === "paper"){
+            return "Scissors wins!";
         }
     }
 };
+
+compare(userChoice,computerChoice);
